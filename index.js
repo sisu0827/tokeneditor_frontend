059ae3730bcc6e07bd98afb3ac47fa149b3aef57
pydeus <typicaladmsky@gmail.com>
2018-09-05 15:03:22 +0300

    Changed port to 8080

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('*', (req,res) =>{
 	res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port);
 
 console.log('App is listening on port ' + port);
